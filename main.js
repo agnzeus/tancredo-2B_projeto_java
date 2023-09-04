19bbab71fc4f89750fbc15587ccaff7047878a09
João Felipe Basso da Silva <116179614+agnzeus@users.noreply.github.com>
2023-09-04 10:49:01 -0300

    04/09

---
+++ main.js
@@ -0,0 +1,18 @@
+function tocaSomSplash(){
+    document.querySelector('#som_tecla_splash').play()
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash; 
+
+
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play()
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomTom(){
+    document.querySelector('#som_tecla_tom').play()
+}
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
